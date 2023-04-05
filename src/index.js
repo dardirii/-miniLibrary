48a7ef58b8a93a3fbdec9472643dfc716c4b7cda
unknown <dardirii>
2023-04-05 14:52:31 +0700

    revisi

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ class Table {
     columns: ["Tittle", "Description"],
     data: [
       ["iPhone 9", "An apple mobile which is nothing like apple"],
-      ["iPhone X", "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip"]
+      ["iPhone X", "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip"],
       ["Huawei P30", "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK"]
     ]
   });
